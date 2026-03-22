827203a5147737501cced21f937570a5550d856d
Chansur25 <surabhichanchal25@gmail.com>
2026-03-22 12:03:07 -0500

    Search Agent

--- main.py
+++ main.py
@@ -5,10 +5,23 @@ from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_tavily import TavilySearch
 from tavily import TavilyClient
+from typing import List
+from pydantic import BaseModel, Field
 import os
 
 load_dotenv()
 tavily = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
+
+class Source(BaseModel):
+    """Scheme for a Source used by agent"""
+    url:str = Field(description="The url of the source")
+    
+class AgentResponse(BaseModel):
+    """Scheme for the agent response"""
+    answer:str = Field(description="The agent's answer to the query")
+    sources:List[Source] = Field(default_factory= list ,
+    description="The list of sources used to generate the answer")
+
 
 @tool
 def search(query: str) -> str:
@@ -23,9 +36,10 @@ def search(query: str) -> str:
     data = tavily.search(query=query)
     return data
 
+
 llm = ChatOpenAI(model = "gpt-5")
 tools = [search]
-agent = create_agent(llm, tools)
+agent = create_agent(llm, tools, response_format=AgentResponse)
 
 def main():
     print("Hello from project-langchain!")
